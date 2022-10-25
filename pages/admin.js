31710a41d3ce2a4c0e92cffdf179c8a26384d331
Nils Wittwer <nils.wittwer@bsfh-lernende.ch>
2022-10-25 21:56:58 +0200

    Added raw Button for Back

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,20 @@
 import {handleShowHeader} from "./_app";
+import defaultStyles from "./stylesheet/global.module.css"
+import adminStyles from "./stylesheet/admin.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRightToBracket} from "@fortawesome/free-solid-svg-icons";
 
 export default function AdminPage () {
 
     handleShowHeader()
 
     return (
-        <div>
-
+        <div className={adminStyles.adminWrapper}>
+            <div>
+                <button className={`${defaultStyles.buttonFilledAutoWidth} ${defaultStyles.buttonFilled}`}><FontAwesomeIcon icon={faRightToBracket} size={"1x"} className={adminStyles.faRightToBracket}/>Leave Admin-Portal to Shop</button>
+                <h1 className={adminStyles.adminPageTitle}>Administration-Portal</h1>
+            </div>
+            
         </div>
     )
 }
