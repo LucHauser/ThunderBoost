334725db342e4087ec7cc9d4510aca8317656406
Nils Wittwer <nils.wittwer@bsfh-lernende.ch>
2022-12-08 18:49:31 +0100

    Optimization Page from discount Settings

--- pages/adminPortal/productManagement/[id]/discountSettings.js
+++ pages/adminPortal/productManagement/[id]/discountSettings.js
@@ -2,6 +2,7 @@ import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import {getProductById} from "@lib/api";
 import defaultStyles from "../../../stylesheet/global.module.css"
+import discountSettingsStyles from "../../../stylesheet/discountSettings.module.css"
 import {Form, FormGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLeftLong} from "@fortawesome/free-solid-svg-icons";
@@ -50,35 +51,61 @@ export default function ProductDiscountManager({session}) {
             </button>
             <h1>Edit Discount for {product?.name}</h1>
             <div className={`${defaultStyles.formSeparatorLine}`} style={{marginTop: 10}}/>
-            <Form onSubmit={saveDiscountSettings} style={{display: "grid", gridTemplateColumns: "50% 50%", columnGap: 10}}>
-                <FormGroup className={defaultStyles.formGroup} style={{display: "flex", gridRow: "1 / 1", gridColumn: "1 / span 2"}}>
-                    <Form.Control className={defaultStyles.formCheckbox} type={"checkbox"} name={"discountActive"} onChange={onProductChange} defaultChecked={product.discountActive}/>
+            <Form onSubmit={saveDiscountSettings} className={discountSettingsStyles.formWrapper}>
+                <FormGroup id={discountSettingsStyles["discountActive"]} className={defaultStyles.formGroup}>
+                    <Form.Control
+                        className={defaultStyles.formCheckbox}
+                        type={"checkbox"}
+                        name={"discountActive"}
+                        onChange={onProductChange}
+                        defaultChecked={product?.discountActive}
+                    />
                     <Form.Label className={defaultStyles.formLabel}>Enable Price Discount</Form.Label>
                 </FormGroup>
-                <Form.Group className={defaultStyles.formGroup} style={{gridRow: "2 / 2", gridColumn: "1 / 1"}}>
+                <Form.Group id={discountSettingsStyles["discountPercent"]} className={defaultStyles.formGroup}>
                     <Form.Label className={defaultStyles.formLabel}>Discount percent</Form.Label>
-                    <Form.Control type={"number"} className={defaultStyles.formInputField} name={"discountPercent"} onChange={onProductChange}/>
+                    <Form.Control
+                        type={"number"}
+                        className={defaultStyles.formInputField}
+                        name={"discountPercent"}
+                        onChange={onProductChange}
+                    />
                 </Form.Group>
-                <Form.Group className={defaultStyles.formGroup} style={{gridRow: "3 / 3", gridColumn: "1 / 1"}}>
+                <Form.Group id={discountSettingsStyles["dateFrom"]} className={defaultStyles.formGroup}>
                     <Form.Label className={defaultStyles.formLabel}>Discount From</Form.Label>
-                    <Form.Control type={"datetime-local"} className={defaultStyles.formInputField} name={"discountFrom"} onChange={onProductChange}/>
+                    <Form.Control
+                        type={"datetime-local"}
+                        className={defaultStyles.formInputField}
+                        name={"discountFrom"}
+                        onChange={onProductChange}
+                    />
                 </Form.Group>
-                <Form.Group className={defaultStyles.formGroup} style={{gridRow: "3 / 3", gridColumn: "2 / 2"}}>
+                <Form.Group id={discountSettingsStyles["dateUntil"]} className={defaultStyles.formGroup}>
                     <Form.Label className={defaultStyles.formLabel}>Discount until</Form.Label>
-                    <Form.Control type={"datetime-local"} className={defaultStyles.formInputField} name={"discountUntil"} onChange={onProductChange}/>
+                    <Form.Control
+                        type={"datetime-local"}
+                        className={defaultStyles.formInputField}
+                        name={"discountUntil"}
+                        onChange={onProductChange}
+                    />
                 </Form.Group>
-                <FormGroup className={defaultStyles.formGroup} style={{display: "flex", gridRow: "4 / 4", gridColumn: "1 / 1"}}>
-                    <Form.Control className={defaultStyles.formCheckbox} type={"checkbox"} onChange={(e) => setProduct({...product, showDiscountUntilDate: e.target.checked})} defaultChecked={product.showDiscountUntilDate}/>
+                <FormGroup id={discountSettingsStyles["showDiscountUntilDate"]} className={defaultStyles.formGroup}>
+                    <Form.Control
+                        className={defaultStyles.formCheckbox}
+                        type={"checkbox"}
+                        onChange={(e) => setProduct({...product, showDiscountUntilDate: e.target.checked})}
+                        defaultChecked={product?.showDiscountUntilDate}
+                    />
                     <Form.Label className={defaultStyles.formLabel}>Show the Until Date of this discount</Form.Label>
                 </FormGroup>
                 {
-                    product.showDiscountUntilDate ?
+                    product?.showDiscountUntilDate ?
                         <>
-                            <Form.Group className={defaultStyles.formGroup} style={{gridRow: "5 / 5", gridColumn: "1 / 1"}}>
+                            <Form.Group id={discountSettingsStyles["discountUntilText"]} className={defaultStyles.formGroup}>
                                 <Form.Label className={defaultStyles.formLabel}>Discount Until Text</Form.Label>
                                 <Form.Control style={{width: "100%"}} className={defaultStyles.formInputField} name={"discountUntilText"} onChange={onProductChange} defaultValue={product.discountUntilText}/>
                             </Form.Group>
-                            <Form.Group className={defaultStyles.formGroup} style={{gridRow: "6 / 6", gridColumn: "1 / 1", display: "flex"}}>
+                            <Form.Group id={discountSettingsStyles["enableCountdown"]} className={defaultStyles.formGroup}>
                                 <Form.Control className={defaultStyles.formCheckbox} type={"checkbox"} onChange={(e) => setProduct({...product, enableCountdown: e.target.checked})} defaultChecked={product.enableCountdown}/>
                                 <Form.Label className={defaultStyles.formLabel}>Enable countdown</Form.Label>
                             </Form.Group>
@@ -87,6 +114,10 @@ export default function ProductDiscountManager({session}) {
                          :
                         null
                 }
+                <div className={discountSettingsStyles.buttonGroup}>
+                    <button className={`${defaultStyles.buttonFilled} ${defaultStyles.buttonFilledAutoWidth}`}>Save</button>
+                    <button className={`${defaultStyles.buttonFilledAutoWidth} ${defaultStyles.buttonFilled} ${defaultStyles.buttonTransparent}`}>Cancel</button>
+                </div>
             </Form>
         </div>
     )
