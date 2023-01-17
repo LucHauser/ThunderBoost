82a61ce7d95c0d79f7e5bf6fd32070f5476adb9c
LucHauser <luc.hauser@bsfh-lernende.ch>
2023-01-17 19:36:53 +0100

    Added about

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,80 @@
+import defaultStyles from "./stylesheet/global.module.css";
+import {Carousel, CarouselItem, Col, Container, Row} from "react-bootstrap";
+
 export default function AboutPage() {
     return (
-        <div>
-            
+        <div className={defaultStyles.AboutPage}>
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <h1 className={defaultStyles.pageTitle}>About Us</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div style={{width: "100%", height: 360, background: "#F784D6"}}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h2 className={defaultStyles.pageSubtitle}>Our Vison</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p className={defaultStyles.defaultStyles}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et et
+                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+                        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui 
+                        blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={6}>
+                        <div style={{width: "100%", height: 360, background: "#F784D6"}}/>
+                    </Col>
+                    <Col lg={6}>
+                        <h2 className={defaultStyles.pageSubtitle}>Our Misson</h2>
+                        <p className={defaultStyles.defaultStyles}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et et
+                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. invidunt ut labore et dolore magna aliquyam
+                        erat, sed diam voluptua.
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={6}>
+                        <h2 className={defaultStyles.pageSubtitle}>The Team</h2>
+                        <p className={defaultStyles.defaultStyles}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et et
+                        <div style={{width: "100%", height: 360, background: "#F784D6"}}/>
+                        </p>
+                    </Col>
+                    <Col lg={6}>
+                        <h2 className={defaultStyles.pageSubtitle}>Our company-history</h2>
+                        <p className={defaultStyles.defaultStyles}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et et
+                        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
